1c06a92078408523e29101112bee2e2b383601dc
rmarduly <omegar321@gmail.com>
2020-11-23 03:24:03 +0100

    try correct errors

--- trainers/twoTower.py
+++ trainers/twoTower.py
@@ -143,13 +143,13 @@ def crossValidation(filenames, k, learningRate, optimiser, loss, epoch, embNum, 
 		
 		#training
 		print("training")
-		model.fit(trainSetCached, epochs = epoch)
+		#model.fit(trainSetCached, epochs = epoch)
 		
 		#testing
 		print("testing")
 		#topk = topKRatings(k, model, usersId, matId, "two tower")
 		model.setCandidates(tf.data.Dataset.from_tensor_slices(matId), k)
-		topk = model(tf.data.Dataset.from_tensor_slices(usersId))
+		topk = model.predict(tf.data.Dataset.from_tensor_slices(usersId))
 		print(topk.numpy())
 		res.append(topKMetrics(topk, [(str(int(i["CUSTOMER_ID"].numpy())), str(int(i["MATERIAL"].numpy()))) for i in testSet], usersId, matId))
 		print(res[-1])
