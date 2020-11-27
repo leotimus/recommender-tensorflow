ce4edbda27053513f41689d6e1da11c187759b24
Jitka Polaskova <jitka.polaskova753@gmail.com>
2020-11-27 15:07:53 +0100

    K

--- trainers/topKmetrics.py
+++ trainers/topKmetrics.py
@@ -2,23 +2,27 @@
 import pandas as pd
 import tensorflow as tf
 from multiprocessing import Pool
+import numpy as np
+import time as time
 
 # To do parallelism using multiprocessing, lambda functions aren't allowed
 # and I can't find an easy way to accept multiple arguments, so the other 
 # two just go in global variables.  ~ asg h
 __currentModel = None
 __currentUserId = None
-def __predictForCurrentUser(i):
-	return (__currentModel.predict([pd.array([__currentUserId]), pd.array([i])]), i)
+var = {"__currentModel":None, "__currentUserId":None}
+
+
 
 def topKRatings(k, model, usersId, itemsId, mtype=None):
 	global __currentModel
 	global __currentUserId
+	global var
 	topK = []
 	isTwoTower = mtype == "two tower"
 	count = 1
 	itemslst = [i for i in itemsId]
-	__currentModel = model
+	var["__currentModel"] = model
 	for u in usersId:
 		print("\rComputing top"+str(k)+": "+str(count)+"/"+str(len(usersId)), end="")
 		ratings = []
@@ -27,14 +31,25 @@ def topKRatings(k, model, usersId, itemsId, mtype=None):
 			ratings = list(model.predict(tf.data.Dataset.from_tensor_slices(dict(candidate)).batch(len(itemsId))))
 			ratings = [(ratings[i], itemsId[i]) for i in range(len(itemsId))]
 		else:
-			__currentUserId = u
-			with Pool() as p:
-				ratings = p.map(__predictForCurrentUser, itemsId)
+			var["__currentUserId"] = u
+			for item in itemsId:
+				ratings.append( __predictForCurrentUser(item))
+		#	with Pool() as p:
+		#		ratings = p.map(__predictForCurrentUser, itemsId)
+		sort_time = time.time()
 		ratings.sort(reverse=True, key=(lambda x: x[0]))
+		print(time.time() - sort_time)
+		#
 		topK.append((u, ratings[:k]))
 		count += 1
 	print("")
 	return topK
+
+def __predictForCurrentUser(i):
+	global __currentModel
+	global var
+#	print([__currentUserId], [i])
+	return (var["__currentModel"].predict([np.array([var["__currentUserId"]]), np.array([i])]), i)
 
 def topKMetrics(predictions, positives, usersId, itemsId):
 	nbrUser = len(usersId)
