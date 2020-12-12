8b10626f06928d02530db052d7cccb5463860a59
Asger HB <asgerhb@gmail.com>
2020-12-12 13:46:57 +0100

    Status update during topk-evaluation

--- trainers/SVD.py
+++ trainers/SVD.py
@@ -430,10 +430,13 @@ def do_topk(user_matrix, item_matrix, test_idset, train_idset, user_ids, item_id
     for batch in np.array_split(user_matrix, TOPK_BATCH_SIZE):
         tensor_batch = tf.convert_to_tensor(batch, dtype=np.float32)
         raw_predictions = topk_predicter(tensor_batch)
+        print_verbose(f"Batch {batch}...")
         for u in range(len(batch)):
             predictions.append((user_id, [(raw_predictions[0][u][j], raw_predictions[1][u][j].numpy()) for j in range(len(raw_predictions[0][u]))]))
             user_id+=1
     
+    clear_verbose_print()
+
     test_set_result = topk.topKMetrics(predictions, test_idset, actual_user_ids, actual_item_ids)
     train_set_result = topk.topKMetrics(predictions, train_idset, actual_user_ids, actual_item_ids)
 
